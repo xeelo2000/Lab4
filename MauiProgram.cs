1926c6bde189ff6542427f2c6645b51c2463d95b
Xee Lo <xee.lo.2000@gmail.com>
2022-10-19 20:46:47 -0500

    added comments

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,6 +2,13 @@
 
 public static class MauiProgram
 {
+    /**
+	Name: XEE LO
+	Date: 10/19/2022
+	Description: LAB 4
+	Bugs: None that i know of
+	Reflection: learned how to unit test
+	*/
     public static IBusinessLogic ibl = new BusinessLogic();
     public static MauiApp CreateMauiApp()
 	{
